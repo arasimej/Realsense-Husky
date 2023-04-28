a27e004e09a8a31a872de3c20a0e7a4d005e8a40
Emily <arasimej@gmail.com>
2023-04-27 21:27:22 -0400

    Edited publisher

--- measure_object_distance/husky_reaction.py
+++ measure_object_distance/husky_reaction.py
@@ -14,37 +14,19 @@ rs = RealsenseCamera()
 mrcnn = MaskRCNN()
 
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-readings = Twist()
-
-def callback(data):
-    # REALSENSE OBJECT DETECTION STUFF
-    ret, bgr_frame, depth_frame = rs.get_frame_stream()
-
-    boxes, classes, contours, centers = mrcnn.detect_objects_mask(bgr_frame)
-    bgr_frame = mrcnn.draw_object_mask(bgr_frame)
-    mrcnn.draw_object_info(bgr_frame, depth_frame)
-
-    # THE DOT
-    point_x, point_y = 250, 188
-    distance_mm = depth_frame[point_x, point_y]
-
-    cv2.circle(bgr_frame, (point_x, point_y), 8, (0, 0, 255), -1)
-
-    cv2.imshow("depth frame", depth_frame)
-    cv2.imshow("Bgr frame", bgr_frame)
+movements = Twist()
 
 def main():
-    global pub
-    global ret 
-    global bgr_frame
-    global depth_frame
-    global boxes 
-    global classes 
-    global contours 
-    global centers 
+    # global pub
+    # global ret 
+    # global bgr_frame
+    # global depth_frame
+    # global boxes 
+    # global classes 
+    # global contours 
+    # global centers 
 
-    while True:
-
+    #while True:
         ret, bgr_frame, depth_frame = rs.get_frame_stream()
 
         # CENTER
@@ -74,49 +56,48 @@ def main():
         cv2.imshow("depth frame", depth_frame)
         cv2.imshow("Bgr frame", bgr_frame)
 
-        # HUSKY REACTION MOVEMENT.
+        # HUSKY REACTION
         for class_id in mrcnn.obj_classes:
             if centers is not None:     
-                if mrcnn.classes[int(class_id)] == "bottle":
-                    if centers[0][0] <= 160:
-                            print("Object Detected Left") 
-                            readings.linear.x = 0
-                            readings.angular.z = 0.2
-                            pub.publish(readings)
-                            print(" ")
-                    if (centers[0][0] > 160 and centers[0][0] < 480):
-                            print("Object Detected Center")
-                            readings.linear.x = 0.2
-                            readings.angular.z = 0
-                            pub.publish(readings)
-                            print(" ")
-                    if centers[0][0] >= 480:
-                            print("Object Detected Right")
-                            readings.linear.x = 0
-                            readings.angular.z = -0.2
-                            pub.publish(readings)
-                            print(" ")
-                    else:
-                            print("No Bottle Seen")
-                            readings.linear.x = 0
-                            readings.angular.z = 0
-                            pub.publish(readings)
-                            print(" ")
+                    if mrcnn.classes[int(class_id)] == "bottle":
+                            if centers[0][0] <= 160:
+                                    print("Object Detected Left") 
+                                    movements.linear.x = 0
+                                    movements.angular.z = 0.2
+                                    pub.publish(movements)
+                                    print(" ")
+                            if (centers[0][0] > 160 and centers[0][0] < 480):
+                                    print("Object Detected Center")
+                                    movements.linear.x = 0.2
+                                    movements.angular.z = 0
+                                    pub.publish(movements)
+                                    print(" ")
+                            if centers[0][0] >= 480:
+                                    print("Object Detected Right")
+                                    movements.linear.x = 0
+                                    movements.angular.z = -0.2
+                                    pub.publish(movements)
+                                    print(" ")
+                            else:
+                                    print("No Bottle Seen")
+                                    movements.linear.x = 0
+                                    movements.angular.z = 0
+                                    pub.publish(movements)
+                                    print(" ")
 
         print(" ")
 
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
-                  
+        # key = cv2.waitKey(1)
+        # if key == 27:
+        #     break
+
 if __name__ == '__main__':
-    rospy.init_node('listener', anonymous=True)
-    sub = rospy.Subscriber("/sonar", PointCloud, callback)
+    rospy.init_node('Stall')
     try:
         while not rospy.is_shutdown():
             main()
     except rospy.ROSInterruptException:
-        pass  
+        pass 
 
 
 
